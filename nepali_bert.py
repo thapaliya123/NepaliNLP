b167be00a9e0ce1f9437947d6104697f12fc8f80
thapaliya123 <thapaliyanish123@gmail.com>
2023-03-23 21:42:58 +0545

    nepali bert ner experimentation

--- nepali_bert.py
+++ nepali_bert.py
@@ -1,11 +1,19 @@
 from transformers import BertModel, BertTokenizer, BertForMaskedLM
 from transformers import pipeline
 
+
+test_sentence_ner = "बनेपाली ले चित्र बनाइरहेका बेला स्थानीय ले गोलो घेरा बनाएर आफ्नो चोक लाई क्यानभास मा हेर्दै थिए । " 
+
 vocab_file_dir = './NepaliBERT/' 
 tokenizer = BertTokenizer.from_pretrained(vocab_file_dir,
                                           strip_accents=False,
                                          clean_text=False )
 
+
+print(dir(tokenizer))
+print(tokenizer(test_sentence_ner))
+
+exit()
 model = BertForMaskedLM.from_pretrained('./NepaliBERT')
 
 fill_mask = pipeline(
